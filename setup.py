d1ca8a8d08daaf75410239cdab5bcad2d27e7848
Hari prasad <hariprasad9693@gmail.com>
2024-08-15 19:37:52 +0530

    intial commit with cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project will help you to predict weather student gets admission or not based on the given data',
+    author='hari prasad',
+    license='',
+)
